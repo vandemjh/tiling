3a21ef6999a4de980d7ee93bb7b509f7eb79df60
vandemjh <jack.vandem@gmail.com>
2020-03-19 12:28:52 -0400

    updated generateImages to work with heroku

--- tests/generateImages.py
+++ tests/generateImages.py
@@ -4,6 +4,6 @@ import pathlib
 for i in range(5500):
     if (i % 250 == 0 and i != 0):
         img = Image.new("RGB", (i, i), color = "red")
-        img.save(str(pathlib.Path().absolute()) + "/images/" + str(i) + ".png")
+        img.save("images/" + str(i) + ".png")
 
 print("success")
